9535407230b5d30ce8bcb84160f9faa61e9db8fc
fgliuyt76 <116411738+fgliuyt76@users.noreply.github.com>
2022-10-21 18:18:43 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 PORT=80
-UUID=5aaed9b7-7fe3-47c3-bb52-db59859ce198
+UUID=04a4448a-278a-4495-99ec-620e563d7868
 WebPage=https://bing.com
 CaddyConfig=https://raw.githubusercontent.com/bsefwe/Rail/main/etc/Caddyfile
 XRayConfig=https://raw.githubusercontent.com/bsefwe/Rail/main/etc/xray.json
